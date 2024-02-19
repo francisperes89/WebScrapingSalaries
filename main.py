19e712c9347cc442e82ceea4e4d5519740669ed1
francisperes89 <francis.po@hotmail.com>
2024-02-19 23:09:03 +0000

    Added selenium to finish the web scraping in all the pages

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import ElementClickInterceptedException
+
 
 def scrape_payscale_data(url):
     all_data = []
+    # Set up Chrome WebDriver
+    chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    chrome_options.add_experimental_option("detach", True)
+    #service = ChromeService("path/to/chromedriver")
+    driver = webdriver.Chrome(options=chrome_options)
 
-    while url:
-        # Send a GET request to the URL
-        response = requests.get(url)
+    try:
+        driver.get(url)
+        time.sleep(2)  # Add a small delay to allow dynamic content to load
+        cookie_button = driver.find_element(By.XPATH, "//*[@id='onetrust-accept-btn-handler']")
+        cookie_button.click()
+        page_counter = 1
+        pages_element = driver.find_element(By.XPATH, "//*[@id='__next']/div/div[1]/article/div[3]/a[6]/div")
+        number_of_pages = int(pages_element.text)
 
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
+        while True:
             # Parse the HTML content of the page
-            soup = BeautifulSoup(response.content, 'html.parser')
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
 
             # Find the table containing the data
             table = soup.find('table')
@@ -29,15 +44,19 @@ def scrape_payscale_data(url):
             all_data.extend(data_rows)
 
             # Check if there is a next page
-            next_page_link = soup.find('a', {'class': 'pagination__next-btn'})
-            if next_page_link:
-                url = f"https://www.payscale.com{next_page_link['href']}"
-            else:
-                url = None
 
-        else:
-            print("Failed to retrieve the webpage. Status code:", response.status_code)
-            break
+            next_page_link = driver.find_element(By.CLASS_NAME, 'pagination__next-btn')
+            if page_counter < number_of_pages:
+                next_page_link.click()
+                page_counter += 1
+                print(page_counter)
+                time.sleep(3)
+            else:
+                break
+        time.sleep(5)
+
+    finally:
+        driver.quit()
 
     # Create a DataFrame from all the collected data
     if all_data:
@@ -45,6 +64,7 @@ def scrape_payscale_data(url):
         return df
     else:
         return None
+
 
 # URL of the first page with the table
 url = "https://www.payscale.com/college-salary-report/majors-that-pay-you-back/bachelors"
